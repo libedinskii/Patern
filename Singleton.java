3f40ca14d650c336997ab17d935547c6d75f704f
libedinskii <124511733+libedinskii@users.noreply.github.com>
2024-06-20 17:56:39 +0900

    Create Singleton.java

---
+++ Singleton.java
@@ -0,0 +1,20 @@
+package com.example.root;
+
+public class Singleton {
+    private static Singleton instance;
+
+    private Singleton() {
+        // private constructor to prevent instantiation
+    }
+
+    public static Singleton getInstance() {
+        if (instance == null) {
+            instance = new Singleton();
+        }
+        return instance;
+    }
+
+    public void showMessage(){
+        System.out.println("Hello World!");
+    }
+}
